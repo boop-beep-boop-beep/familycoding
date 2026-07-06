ed786cbc6f99dee25499c0db8cfc78f15c7fe0c6
Dad <dad@dad.com>
2026-07-05 21:19:11 -0400

    Test babbler with different sources and word-memory sizes
    Worked on the babbler by looking through different sources and seeing how it reacts. Also switched the babbler from single words to pairs of words to three words and back to pairs of words.

--- projects/how-ai-works/babbler.py
+++ projects/how-ai-works/babbler.py
@@ -10,23 +10,27 @@ for word in words:
     cleaned_words.append(word.strip(punctuation))
 words = cleaned_words
 lookup = {}
-for i in range(len(words) - 1):
-    current_word = words[i]
-    next_word = words[i + 1]
-    if current_word not in lookup:
-        lookup[current_word] = []
-    lookup[current_word].append(next_word)
-    current_word = random.choice(words)
-sentence = [current_word]
+for i in range(len(words) - 2):
+    current_pair = (words[i], words[i + 1])
+    next_word = words[i + 2]
+    if current_pair not in lookup:
+        lookup[current_pair] = []
+    lookup[current_pair].append(next_word)
 
-for i in range(20):
-    if current_word not in lookup:
+start = random.randint(0, len(words) - 3)
+current_pair = (words[start], words[start + 1])
+sentence = [current_pair[0], current_pair[1]]
+
+for i in range(50):
+    if current_pair not in lookup:
         break
-    next_word = random.choice(lookup[current_word])
+    next_word = random.choice(lookup[current_pair])
     sentence.append(next_word)
-    current_word = next_word
+    current_pair = (current_pair[1], next_word)
 
 #print(len(words))
 #print(words[:25])
 #print(lookup[])
-print(" ".join(sentence))
+sentence[0] = sentence[0][0].upper() + sentence[0][1:]
+output = " ".join(sentence) + "."
+print(output)
